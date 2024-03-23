43e69ad8a3740c1f982c0e61a6edc1cede55beb8
load28 <tjalsdud89@naver.com>
2024-03-23 21:27:37 +0900

    unsubscribe socekt event

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -17,6 +17,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { SocketService } from './socket.service';
 import { Todo } from './api/get-todo-list';
+import { Subscription } from 'rxjs';
 
 const provideQuery = (qc: QueryClient) => {
   return makeEnvironmentProviders([
@@ -26,6 +27,7 @@ const provideQuery = (qc: QueryClient) => {
       multi: true,
       useValue: () => {
         qc.mount();
+        const map = new Map<string, Subscription[]>();
 
         // todo 아래 동작들을 추상화 하여 각 소켓 별로 기능을 별도로 만들기
         const socketService = inject(SocketService);
@@ -33,33 +35,40 @@ const provideQuery = (qc: QueryClient) => {
           switch (cache.type) {
             case 'added': {
               if (
+                cache.query.meta?.['month'] &&
                 cache.query.queryKey[0] === 'todoList' &&
-                cache.query.meta?.['month']
+                cache.query.queryKey[1] === cache.query.meta['month']
               ) {
                 socketService.joinTodoListRoom(
                   cache.query.meta['month'] as string,
                 );
-                socketService.onTodoAdded().subscribe((data: Todo) => {
-                  qc.setQueryData(
-                    ['todoList', cache.query.meta!['month']],
-                    (old: Todo[] | undefined) => {
-                      const safeOld = old ?? [];
-                      return [data, ...safeOld];
-                    },
-                  );
-                });
-                socketService.onTodoRemoved().subscribe((id: string) => {
-                  qc.setQueryData(
-                    ['todoList', cache.query.meta!['month']],
-                    (old: Todo[] | undefined) => {
-                      const safeOld = old ?? [];
-                      return safeOld.filter((todo) => todo.id !== id);
-                    },
-                  );
-                });
+                const sub1 = socketService
+                  .onTodoAdded()
+                  .subscribe((data: Todo) => {
+                    qc.setQueryData(
+                      ['todoList', cache.query.meta!['month']],
+                      (old: Todo[] | undefined) => {
+                        const safeOld = old ?? [];
+                        return [data, ...safeOld];
+                      },
+                    );
+                  });
+                const sub2 = socketService
+                  .onTodoRemoved()
+                  .subscribe((id: string) => {
+                    qc.setQueryData(
+                      ['todoList', cache.query.meta!['month']],
+                      (old: Todo[] | undefined) => {
+                        const safeOld = old ?? [];
+                        return safeOld.filter((todo) => todo.id !== id);
+                      },
+                    );
+                  });
+                map.set(cache.query.meta['month'] as string, [sub1, sub2]);
               } else if (
+                cache.query.meta?.['id'] &&
                 cache.query.queryKey[0] === 'todo' &&
-                cache.query.meta?.['id']
+                cache.query.queryKey[1] === cache.query.meta['id']
               ) {
                 socketService.joinTodoRoom(cache.query.meta['id'] as string);
               }
@@ -67,15 +76,20 @@ const provideQuery = (qc: QueryClient) => {
             }
             case 'removed': {
               if (
+                cache.query.meta?.['month'] &&
                 cache.query.queryKey[0] === 'todoList' &&
-                cache.query.meta?.['month']
+                cache.query.queryKey[1] === cache.query.meta['month']
               ) {
                 socketService.leaveTodoListRoom(
                   cache.query.meta['month'] as string,
                 );
+                map.get(cache.query.meta['month'] as string)?.forEach((sub) => {
+                  sub.unsubscribe();
+                });
               } else if (
+                cache.query.meta?.['id'] &&
                 cache.query.queryKey[0] === 'todo' &&
-                cache.query.meta?.['id']
+                cache.query.queryKey[1] === cache.query.meta['id']
               ) {
                 socketService.leaveTodoRoom(cache.query.meta['id'] as string);
               }
